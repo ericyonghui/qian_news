ebdfa2141cf2bdca3aee034ff7df3aa4a5c024ca
yonghui_love@yeah.net <qian_news8888>
2020-08-12 15:09:14 +0800

    mobile search

--- src/page/searchMobile/searchMobile.js
+++ src/page/searchMobile/searchMobile.js
@@ -1,11 +1,25 @@
 import React from "react";
 import router from "umi/router";
 import { Link } from "react-router-dom";
-import { CloseCircleOutlined } from "@ant-design/icons";
+import { CheckCircleFilled, CloseOutlined } from "@ant-design/icons";
 import { Select } from "antd";
 
 import Header from "../../components/header/header";
 import style from "./searchMobile.less";
+// import style from "./header.less";
+
+import img1 from "../IMG/季节/季节-2020早春.png";
+import img2 from "../IMG/季节/季节-2020早秋.jpg";
+import img3 from "../IMG/季节/季节-2020春夏.jpg";
+import img4 from "../IMG/季节/季节-2020秋冬.jpg";
+import img5 from "../IMG/性别/性别-女装.jpg";
+import img6 from "../IMG/性别/性别-男装.png";
+import img7 from "../IMG/性别/性别-童装.jpg";
+import img8 from "../IMG/类别/类别-T台.png";
+import img9 from "../IMG/类别/类别-图案.jpg";
+import img10 from "../IMG/类别/类别-手绘.png";
+import img11 from "../IMG/类别/类别-款式.jpg";
+import img12 from "../IMG/类别/类别-街拍.png";
 
 export default class searchMobile extends React.Component {
   render() {
@@ -13,100 +27,187 @@ export default class searchMobile extends React.Component {
       <div>
         <Header />
         <div className={style.container}>
-          <div className={style.searchHistory}>
-            <span>最近搜索</span>
-            <CloseCircleOutlined className={style.ICON} />
+          {/* 最近搜索 */}
+          <div className={style.searchBorder}>
+            <div className={style.searchHistory}>
+              <span>最近搜索</span>
+            </div>
+            <ul className={`${style.searchHistoryList} ${style.clearfix}`}>
+              <li>
+                男装 &nbsp;&nbsp;&nbsp;&nbsp;
+                <CloseOutlined className={style.ICON} />
+              </li>
+              <li>
+                T台 &nbsp;&nbsp;&nbsp;&nbsp;
+                <CloseOutlined className={style.ICON} />
+              </li>
+            </ul>
           </div>
-          <ul className={`${style.searchHistoryList} ${style.clearfix}`}>
-            <li>milan</li>
-            <li>taotou</li>
-          </ul>
+          {/* 已选条件 */}
+          <div className={style.searchBorder}>
+            <div className={style.searchHistory}>
+              <span>已选条件</span>
+            </div>
+            <ul className={`${style.searchHistoryList} ${style.clearfix}`}>
+              <li>
+                男装 &nbsp;&nbsp;&nbsp;&nbsp;
+                <CloseOutlined className={style.ICON} />
+              </li>
+              <li>
+                T台 &nbsp;&nbsp;&nbsp;&nbsp;
+                <CloseOutlined className={style.ICON} />
+              </li>
+            </ul>
+          </div>
+          {/* 性别 */}
           <div className={`${style.ulList} ${style.clearfix}`}>
             <h4>性别</h4>
             <ul>
               <li>
-                <img src="" />
-                <span>男装</span>
+                <img src={img6} />
+                <p>男装</p>
+                <div className={`${style.markmin} ${style.markminOn}`}></div>
+                <CheckCircleFilled
+                  className={style.checkOn}
+                  style={{ display: "block" }}
+                />
               </li>
               <li>
-                <img src="" />
-                <span>女装</span>
+                <img src={img5} />
+                <p>女装</p>
+                <div className={style.markmin}></div>
+                <CheckCircleFilled className={style.checkOn} />
               </li>
               <li>
-                <img src="" />
-                <span>童装</span>
+                <img src={img7} />
+                <p>童装</p>
+                <div className={style.markmin}></div>
+                <CheckCircleFilled className={style.checkOn} />
               </li>
             </ul>
           </div>
+          {/* 类别 */}
           <div className={`${style.ulList} ${style.clearfix}`}>
             <h4>类别</h4>
             <ul>
               <li>
-                <img src="" />
-                <span>T台</span>
+                <img src={img8} />
+                <p>T台</p>
+                <div className={style.markmin}></div>
+                <CheckCircleFilled className={style.checkOn} />
               </li>
               <li>
-                <img src="" />
-                <span>款式</span>
+                <img src={img11} />
+                <p>款式</p>
+                <div className={style.markmin}></div>
+                <CheckCircleFilled className={style.checkOn} />
               </li>
               <li>
-                <img src="" />
-                <span>街拍</span>
+                <img src={img12} />
+                <p>街拍</p>
+                <div className={style.markmin}></div>
+                <CheckCircleFilled className={style.checkOn} />
               </li>
-              <li>
-                <img src="" />
-                <span>图案</span>
+              <li className={style.pictrueP}>
+                <img src={img9} />
+                <p>图案</p>
+                <div className={style.markmin}></div>
+                <CheckCircleFilled className={style.checkOn} />
               </li>
-              <li>
-                <img src="" />
-                <span>手绘</span>
+              <li className={style.pictrueP}>
+                <img src={img10} />
+                <p>手绘</p>
+                <div className={style.markmin}></div>
+                <CheckCircleFilled className={style.checkOn} />
               </li>
             </ul>
           </div>
+          {/* 季节 */}
           <div className={`${style.ulList} ${style.clearfix}`}>
             <h4>季节</h4>
-            <ul>
-              <li>
-                <img src="" />
-                <span>2020早春</span>
-              </li>
-              <li>
-                <img src="" />
-                <span>2020春夏</span>
-              </li>
-              <li>
-                <img src="" />
-                <span>2020早秋</span>
-              </li>
-              <li>
-                <img src="" />
-                <span>2020秋冬</span>
-              </li>
-            </ul>
-          </div>
-
-          <div className={style.searchYear}>
-            <h4>年份</h4>
             <Select
               bordered={false}
               maxTagTextLength="100"
-              style={{ width: 120 }}
+              style={{ width: 120, fontWeight: "bold", marginBottom: "20px" }}
               defaultValue="2020"
             >
               <Option value="2020">2020</Option>
               <Option value="2019">2019</Option>
               <Option value="2018">2018</Option>
             </Select>
-          </div>
-          <div className={`${style.searchCountry} ${style.clearfix}`}>
-            <h4>国家</h4>
             <ul>
-              <li>中国</li>
-              <li>日本</li>
+              <li>
+                <img src={img1} />
+                <p>2020早春</p>
+                <div className={style.markmin}></div>
+                <CheckCircleFilled className={style.checkOn} />
+              </li>
+              <li>
+                <img src={img3} />
+                <p>2020春夏</p>
+                <div className={style.markmin}></div>
+                <CheckCircleFilled className={style.checkOn} />
+              </li>
+              <li>
+                <img src={img2} />
+                <p>2020早秋</p>
+                <div className={style.markmin}></div>
+                <CheckCircleFilled className={style.checkOn} />
+              </li>
+              <li>
+                <img src={img4} />
+                <p>2020秋冬</p>
+                <div className={style.markmin}></div>
+                <CheckCircleFilled className={style.checkOn} />
+              </li>
             </ul>
           </div>
+          {/* 国家 */}
+          <div className={`${style.searchCountry} ${style.clearfix}`}>
+            <div className={`${style.searchCountryTitle} ${style.clearfix}`}>
+              <h4>地区</h4>
+              <input type="text" />
+            </div>
+            <ul className={`${style.searchCountryDZ} ${style.clearfix}`}>
+              <li>亚洲</li>
+              <li>欧洲</li>
+              <li>北美洲</li>
+              <li>南美洲</li>
+            </ul>
+            <ul className={`${style.searchCountryGJ}`}>
+              <li className={style.clearfix}>
+                <p>亚洲</p>
+                <ul className={`${style.searchCountryFL} ${style.clearfix}`}>
+                  <li></li>
+                </ul>
+              </li>
+              <li className={style.clearfix}>
+                <p>中国</p>
+                <ul className={`${style.searchCountryFL} ${style.clearfix}`}>
+                  <li className={style.on}>上海</li>
+                  <li>香港</li>
+                </ul>
+              </li>
+              <li className={style.clearfix}>
+                <p>日本</p>
+                <ul className={`${style.searchCountryFL} ${style.clearfix}`}>
+                  <li>东京</li>
+                </ul>
+              </li>
+              <li className={style.clearfix}>
+                <p>韩国</p>
+                <ul className={`${style.searchCountryFL} ${style.clearfix}`}>
+                  <li>首尔</li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          {/* 品牌 */}
           <div className={`${style.searchBrand}`}>
-            <h4>品牌</h4>
+            <div className={`${style.searchBrandTitle} ${style.clearfix}`}>
+              <h4>品牌</h4>
+              <input type="text" />
+            </div>
             <ul className={`${style.searchBrandLetter} ${style.clearfix}`}>
               <li className={style.on}>A</li>
               <li>B</li>
