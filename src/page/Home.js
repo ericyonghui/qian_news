9f1e23bff92d2c43998fd2d2151166828cb31291
eric3315 <wyh3315@163.com>
2020-08-13 18:24:59 +0800

    20200813

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,79 +1,172 @@
 import React, { PureComponent } from "react";
 import { Carousel } from "antd";
+import router from 'umi/router';
+import Link from 'umi/link';
+import {lazyLoad} from '../util/utils';
+import { IMG_HOST,IMG_HOST_IP } from "../util/common";
+import axios from "../util/axios";
 import style from "./home.less";
-import img1 from "./01.png";
-import img2 from "./02.png";
-import img3 from "./03.png";
-import img4 from "./left.png"
-import img5 from "./right.png"
+
 
 class Home extends PureComponent {
     state={
-        list:[
-            {
-                id:'01',
-                src:img1,
-                title:'prada   2020秋冬',
-                time:'2020.6.15'
-            },
-            {
-                id:'02',
-                src:img2,
-                title:'prada   2020秋冬',
-                time:'2020.6.15'
-            },
-            {
-                id:'03',
-                src:img2,
-                title:'prada   2020秋冬',
-                time:'2020.6.15'
-            },
-            {
-                id:'04',
-                src:img3,
-                title:'prada   2020秋冬',
-                time:'2020.6.15'
-            },
-            {
-                id:'05',
-                src:img3,
-                title:'prada   2020秋冬',
-                time:'2020.6.15'
-            },
-            {
-                id:'06',
-                src:img1,
-                title:'prada   2020秋冬',
-                time:'2020.6.15'
-            }
-        ],
-        pictureList:[
-            {
-                id:'01',
-                src:img5
-            },
-            {
-                id:'02',
-                src:img5
-            },
-            {
-                id:'03',
-                src:img5
-            },
-            {
-                id:'04',
-                src:img5
-            },
-            {
-                id:'05',
-                src:img5
-            },
-            {
-                id:'06',
-                src:img5
-            }
-        ]
-    }
+       fashionList:[],
+       styleList:[],
+       materialList:[],
+       manuscriptsList:[],
+    };
+
+  componentDidMount(){
+    this.queryFashionIndividuation();
+    this.queryStyleIndividuation();
+    this.queryMaterialIndividuation();
+    this.queryManuscriptsIndividuation();
+    let imgArr = document.querySelectorAll('img');
+    lazyLoad(imgArr);
+    window.addEventListener('scroll',this.handleImgScroll);
+  }
+  handleImgScroll=async ()=>{
+    let imgArr = document.querySelectorAll('img');
+    await lazyLoad(imgArr);
+  };
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll',this.handleImgScroll);
+  }
+
+  queryFashionIndividuation=()=>{
+    const _this = this;
+    _this.setState({
+      fashionList: []
+    },async ()=>{
+      let result = await axios({
+        method:"GET",
+        url:`/fashion/getFashionIndividuation`,
+      });
+      const { code,data} = result;
+      if (code === 200) {
+        _this.setState({
+          fashionList: data
+        });
+      }
+    })
+
+  };
+  queryStyleIndividuation=()=>{
+    const _this = this;
+    _this.setState({
+      styleList: []
+    },async ()=>{
+      let result = await axios({
+        method:"GET",
+        url:`/style/getStyleIndividuation`,
+      });
+      const { code,data} = result;
+      if (code === 200) {
+        _this.setState({
+          styleList: data
+        });
+      }
+    })
+
+  };
+  queryMaterialIndividuation=()=>{
+    const _this = this;
+    _this.setState({
+      materialList: []
+    },async ()=>{
+      let result = await axios({
+        method:"GET",
+        url:`/material/getMaterialIndividuation`,
+      });
+      const { code,data} = result;
+      if (code === 200) {
+        _this.setState({
+          materialList: data
+        });
+      }
+    })
+
+  };
+  queryManuscriptsIndividuation=()=>{
+    const _this = this;
+    _this.setState({
+      manuscriptsList: []
+    },async ()=>{
+      let result = await axios({
+        method:"GET",
+        url:`/material/getManuscriptsIndividuation`,
+      });
+      const { code,data} = result;
+      if (code === 200) {
+        _this.setState({
+          manuscriptsList: data
+        });
+      }
+    })
+  };
+
+
+  handleFashionSlideShow=(id)=>{
+    router.push(`/slideShow/runway_slideShow?id=${id}`);
+  };
+  renderFashion(){
+    let vDOM=[],{fashionList} = this.state;
+    fashionList.forEach(item=>{
+      let {main_id,fashion_season,release_date,brand_name,gender,img} = item;
+      vDOM.push(<div className={style.trendListItem} key={main_id} onClick={() => {this.handleFashionSlideShow(main_id)}}>
+        <div className={style.trendListItemBack}>
+          <img data-src={`${IMG_HOST}/webp${img}`} alt="" />
+          <h4 title={`${release_date}${fashion_season}${brand_name}${gender === "women" ? "女装" : "男装"}发布会`}>{`${release_date}${fashion_season}${brand_name}${
+            gender === "women" ? "女装" : "男装"
+            }发布会`}</h4>
+          <p>MR Right</p>
+          <p>2020.6.15</p>
+        </div>
+      </div>)
+    });
+    return vDOM;
+  }
+
+  handleStyleSlideShow=(main_id,fashionSeason,fashionRegion,brandName)=>{
+    router.push(`/brand?id=${main_id}&fashion_season=${fashionSeason}&fashion_region=${fashionRegion}&brand=${brandName}`);
+  };
+  renderStyle(){
+    let vDOM=[],{styleList} = this.state;
+    styleList.forEach(item=>{
+      let {main_id,brand_name,fashion_season,fashion_region,gender,img}= item;
+      vDOM.push(<div key={main_id} className={style.item} onClick={() => {this.handleStyleSlideShow(main_id,fashion_season,fashion_region,brand_name)}}>
+        <div className={style.brandItem}>
+          <img  data-src={`${IMG_HOST_IP}${img}`} alt=""/>
+          <p title={`${brand_name}${fashion_season}${gender === "women" ? "女装" : (gender === "men" ? "男装": "童装")}系列`}>{`${brand_name}${fashion_season}${gender === "women" ? "女装" : (gender === "men" ? "男装": "童装")}系列`}</p>
+          <span>{item.time}</span>
+        </div>
+      </div>);
+    });
+    return vDOM;
+  }
+
+  handleMaterialSlideShow=(id)=>{
+    router.push(`/detail?id=${id}`);
+  };
+  renderMaterial(){
+    let vDOM=[],{materialList} = this.state;
+    materialList.forEach(item=>{
+      let {main_id,img} = item;
+      vDOM.push( <li key={main_id} onClick={()=>{this.handleMaterialSlideShow(main_id)}}>
+        <img data-src={`${IMG_HOST_IP}/chosen/${img}`} alt=""/>
+      </li>)
+    });
+    return vDOM;
+  }
+  renderManuscripts(){
+    let vDOM=[],{manuscriptsList} = this.state;
+    manuscriptsList.forEach(item=>{
+      let {main_id,img} = item;
+      vDOM.push( <img data-src={`${IMG_HOST_IP}/chosen/${img}`} alt="" key={main_id} onClick={()=>{this.handleMaterialSlideShow(main_id)}}/>)
+    });
+    return vDOM;
+  }
   render() {
     return (
       <div className={style.homeContainer}>
@@ -100,39 +193,16 @@ class Home extends PureComponent {
             <p>秀场直击，最IN的前沿时尚资讯</p>
           </div>
         <div className={style.trend}>
-         
+
           <div className={style.trendList}>
             <div className={style.trendListContainer}>
-              <div className={style.trendListItem}>
-                <div className={style.trendListItemBack}>
-                  <img src={img1} alt="" />
-                  <h4>2020春夏Chanel高级成衣</h4>
-                  <p>MR Right</p>
-                  <p>2020.6.15</p>
-                </div>
-              </div>
-              <div className={style.trendListItem}>
-                <div className={style.trendListItemBack}>
-                  <img src={img2} alt="" />
-                  <h4> 2020春夏 Alexander McQueen高级成衣</h4>
-                  <p> Honor</p>
-                  <p>2020.4.7</p>
-                </div>
-              </div>
-              <div className={style.trendListItem}>
-                <div className={style.trendListItemBack}>
-                  <img src={img3} alt="" />
-                  <h4>2020秋冬 Louis Vuitton高级成衣</h4>
-                  <p>Clive</p>
-                  <p>2020.5.16</p>
-                </div>
-              </div>
-            </div> 
+              {this.renderFashion()}
+            </div>
           </div>
-          
+
         </div>
         <div className={style.buttonPubilc}>
-                <p>查看全部潮流发布</p>
+                <p><Link to="/runway">查看全部潮流发布</Link></p>
             </div>
          {/* 品牌聚焦 */}
          <div className={style.brand}>
@@ -141,23 +211,10 @@ class Home extends PureComponent {
                 <p>世界品牌，最新潮流，一网打尽</p>
             </div>
             <div className={style.brandList}>
-               
-                    {
-                        this.state.list.map(item=>(
-                            <div key="item.id" className={style.item}>
-                                <div className={style.brandItem}>
-                                    <img src={item.src} alt=""/>
-                                    <p>{item.title}</p>
-                                    <span>{item.time}</span>
-                                </div>
-                                
-                            </div>
-                        ))
-                    }
-                
+                  {this.renderStyle()}
             </div>
             <div className={style.buttonPubilc}>
-                <p>查看全部品牌咨询</p>
+              <p><Link to="/style">查看全部品牌咨询</Link></p>
             </div>
          </div>
         {/* 图案 &amp; 素材 */}
@@ -165,23 +222,15 @@ class Home extends PureComponent {
             <h3>图案 &amp; 素材</h3>
             <div className={`${style.pictureList} ${style.clearfix}`}>
                 <div className={style.pictureLeft}>
-                    <img src={img4} alt=""/>
-                    <p>查看更多素材</p>
+                   {this.renderManuscripts()}
+                  <p><Link to="/material">查看更多素材</Link></p>
                 </div>
                 <div className={`${style.pictureRight} ${style.clearfix}`}>
-                    <ul>
-                        {
-                            this.state.pictureList.map(item=>(
-                                <li key={item.id}>
-                                    <img src={item.src} alt=""/>
-                                </li>
-                            ))
-                        }
-                    </ul>
+                    <ul>{this.renderMaterial()}</ul>
                 </div>
             </div>
             <div className={style.buttonPubilc}>
-                <p>查看更多图案</p>
+              <p><Link to="/design">查看更多图案</Link></p>
             </div>
         </div>
       </div>
